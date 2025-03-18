ca209e796f4fc0304b2b02fe5203665a46f1a5eb
Marli Mota <marli.mota42@gmail.com>
2025-03-18 18:29:04 -0300

    custom textfield

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
 
+import '../components/custom_textfield.dart';
+
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.person,
+                size: 80,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              const SizedBox(height: 25),
+              Text('SOCIAL', style: TextStyle(fontSize: 20)),
+              const SizedBox(height: 50),
+              CustomTextField(
+                hintText: 'Email',
+                obscureText: false,
+                controller: emailController,
+              ),
+              const SizedBox(height: 20),
+              CustomTextField(
+                hintText: 'Senha',
+                obscureText: true,
+                controller: passwordController,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Esqueceu a senha?',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
